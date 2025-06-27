385c99dd4dc357a639f149ee3c3da0e6715ab5d0
devhawkz <skydevhawkz@gmail.com>
2025-06-27 20:26:45 +0200

    Injected IServiceProvider inside GlobalExceptionHandler in order to be able to user Logger service which is scoped service inside GlobalExceptionHandler which is singleton service

--- MailFlow.API/GlobalExceptionHandler.cs
+++ MailFlow.API/GlobalExceptionHandler.cs
@@ -3,29 +3,37 @@ using Entities.ErrorModel;
 using Microsoft.AspNetCore.Diagnostics;
 using System.Net;
 
-namespace MailFlow.API
+namespace MailFlow.API;
+
+public class GlobalExceptionHandler : IExceptionHandler
 {
-    public class GlobalExceptionHandler : IExceptionHandler
+    private readonly IServiceProvider _serviceProvider;
+
+    public GlobalExceptionHandler(IServiceProvider serviceProvider)
     {
-        
+        _serviceProvider = serviceProvider;
+    }
 
-        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        using var scope = _serviceProvider.CreateScope();   
+        var logger = scope.ServiceProvider.GetRequiredService<ILoggerManager>();
+
+
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        httpContext.Response.ContentType = "application/json";
+
+        var contextFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
+        if (contextFeature is not null)
         {
-            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            httpContext.Response.ContentType = "application/json";
-
-            var contextFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
-            if (contextFeature is not null)
+            logger.LogError($"Something went wrong: {exception.Message}");
+            
+            await httpContext.Response.WriteAsync(new ErrorDetails()
             {
-                //_logger.LogError($"Something went wrong: {exception.Message}");
-                
-                await httpContext.Response.WriteAsync(new ErrorDetails()
-                {
-                    StatusCode = httpContext.Response.StatusCode,
-                    Message = "Internal Server Error"
-                }.ToString());
-            }
-            return true;
+                StatusCode = httpContext.Response.StatusCode,
+                Message = "Internal Server Error"
+            }.ToString());
         }
+        return true;
     }
 }
